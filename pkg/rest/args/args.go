283d2d0a1b6daecbc37cf6a6a7b761590aa22ed1
Alexander Sowitzki <dev@eqrx.net>
2019-07-06 13:19:00 +0200

    Add REST package

---
+++ pkg/rest/args/args.go
@@ -0,0 +1,143 @@
+/*
+Copyright 2019 Alexander Sowitzki.
+
+GNU Affero General Public License version 3 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://opensource.org/licenses/AGPL-3.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package args
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+// Argument is an argument that can be collected from an URL.
+type Argument func(*url.URL) error
+
+// Collect all the given arguments.
+func Collect(url *url.URL, arguments []Argument) error {
+	for _, argument := range arguments {
+		if err := argument(url); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func stringFromURL(url *url.URL, name string, optional bool) (string, bool, error) {
+	values, ok := url.Query()[name]
+
+	if !ok && optional {
+		return "", false, nil
+	}
+
+	if !ok {
+		return "", false, fmt.Errorf("URL does not contain argument %v", name)
+	}
+
+	if len(values) > 1 {
+		return "", false, fmt.Errorf("URL contains %v multiple times", name)
+	}
+
+	if len(values) < 1 && !optional {
+		return "", false, fmt.Errorf("URL does not contain %v", name)
+	}
+
+	if len(values) < 1 {
+		return "", false, nil
+	}
+
+	return values[0], true, nil
+}
+
+// Bool represents a bool type argument.
+func Bool(target *bool, name string, optional bool) Argument {
+	return func(url *url.URL) error {
+		rawValue, set, err := stringFromURL(url, name, optional)
+		if !set || err != nil {
+			return err
+		}
+
+		value, err := strconv.ParseBool(rawValue)
+		if err != nil {
+			return fmt.Errorf("Could not parse bool from URL argument %v: %v", name, err)
+		}
+		*target = value
+		return nil
+	}
+}
+
+// Int represents a integer argument.
+func Int(target *int, name string, optional bool) Argument {
+	return func(url *url.URL) error {
+		rawValue, set, err := stringFromURL(url, name, optional)
+		if !set || err != nil {
+			return err
+		}
+
+		value, err := strconv.ParseInt(rawValue, 10, 0)
+		if err != nil {
+			return fmt.Errorf("Could not parse int from URL argument %v: %v", name, err)
+		}
+		*target = int(value)
+		return nil
+	}
+}
+
+// Uint represents an unsigned integer argument.
+func Uint(target *uint, name string, optional bool) Argument {
+	return func(url *url.URL) error {
+		rawValue, set, err := stringFromURL(url, name, optional)
+		if !set || err != nil {
+			return err
+		}
+
+		value, err := strconv.ParseUint(rawValue, 10, 0)
+		if err != nil {
+			return fmt.Errorf("Could not parse int from URL argument %v: %v", name, err)
+		}
+		*target = uint(value)
+		return nil
+	}
+}
+
+// Duration represents an time duration argument.
+func Duration(target *time.Duration, name string, optional bool) Argument {
+	return func(url *url.URL) error {
+		rawValue, set, err := stringFromURL(url, name, optional)
+		if !set || err != nil {
+			return err
+		}
+
+		value, err := time.ParseDuration(rawValue)
+		if err != nil {
+			return fmt.Errorf("Could not parse duration from URL argument %v: %v", name, err)
+		}
+		*target = value
+		return nil
+	}
+}
+
+// String represents a string argument.
+func String(target *string, name string, optional bool) Argument {
+	return func(url *url.URL) error {
+		value, set, err := stringFromURL(url, name, optional)
+		if !set || err != nil {
+			return err
+		}
+		*target = value
+		return nil
+	}
+}
---
+++ pkg/rest/rest.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2019 Alexander Sowitzki.
+
+GNU Affero General Public License version 3 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://opensource.org/licenses/AGPL-3.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rest
+
+import (
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+// Serve creates a REST server secured by TLS with client cert authentication.
+func Serve(listen string, caPath string, crtPath string, keyPath string, mux *http.ServeMux) error {
+	cert, err := tls.LoadX509KeyPair(crtPath, keyPath)
+	if err != nil {
+		return fmt.Errorf("Failed to load TLS cert/key pair from %v & %v: %v", crtPath, keyPath, err)
+	}
+
+	pem, err := ioutil.ReadFile(caPath)
+	if err != nil {
+		return fmt.Errorf("Failed to load CA file from %v: %v", caPath, err)
+	}
+
+	certpool := x509.NewCertPool()
+	if !certpool.AppendCertsFromPEM(pem) {
+		return fmt.Errorf("Failed to parse CA file from %v", caPath)
+	}
+	config := tls.Config{ // Make things "a little" incompatible but secure. Basics taken from https://cipherli.st .
+		Certificates:             []tls.Certificate{cert},
+		ClientCAs:                certpool,
+		Rand:                     rand.Reader,
+		ClientAuth:               tls.RequireAndVerifyClientCert,
+		MinVersion:               tls.VersionTLS12,
+		PreferServerCipherSuites: true,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+		},
+	}
+
+	server := http.Server{
+		Addr:              listen,
+		TLSConfig:         &config,
+		ReadHeaderTimeout: 5 * time.Second,
+		IdleTimeout:       120 * time.Second,
+		Handler:           mux,
+		// Disable HTTP v2.0 since that woul require 128 bit ciphers.
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler)),
+	}
+	return server.ListenAndServeTLS("", "")
+}
